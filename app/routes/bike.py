6d91a62bf7f11fd8ac55acc63794e9e6ca374587
Samantha Hunt <ariaandsam@gmail.com>
2022-10-27 10:41:45 -0700

    added comments

--- app/routes/bike.py
+++ app/routes/bike.py
@@ -32,12 +32,17 @@ def get_all_bikes():
 
 @bike_bp.route("/<bike_id>", methods=["GET"])
 def get_one_bike(bike_id):
+    #see if bike_id can be converted to an integer
+    #try-except: try to convert to an int, if error occurs, catch it and raise 400 error with message
     try:
         bike_id = int(bike_id)
     except ValueError:
         response_str = f"Invalid bike_id: `{bike_id}`. ID must be an integer"
         return jsonify({"message": response_str}), 400
+    #after the try-except: bike_id will be a valid int
 
+    #looping through data to find a bike with matching bike_id
+    #if found: return that bikes data with 200 response code
     for bike in bikes:
         if bike.id == bike_id:
             bike_dict = {
@@ -47,6 +52,9 @@ def get_one_bike(bike_id):
                 "size": bike.size,
                 "type": bike.type
             }
+            #return in the if block
             return jsonify(bike_dict), 200
+
+    #after the loop: the bike with matching bike_id was not found, we will raise a 404 error with message
     response_message = f"bike_id: {bike_id} not found."
     return jsonify({"message": response_message}), 404
