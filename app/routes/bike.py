024a6a37c46edb8f39da639cb6a6910857aa1765
Samantha Hunt <ariaandsam@gmail.com>
2022-11-01 10:10:14 -0700

    classwork 10/31

--- app/routes/bike.py
+++ app/routes/bike.py
@@ -1,9 +1,24 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort, make_response
 from app import db
 from app.models.bike import Bike
 
 
 bike_bp = Blueprint("bike_bp", __name__, url_prefix="/bike")
+
+def get_one_bike_or_abort(bike_id):
+    try:
+        bike_id = int(bike_id)
+    except ValueError:
+        response_str = f"Invalid bike_id: `{bike_id}`. ID must be an integer."
+        abort(make_response(jsonify({"message":response_str}), 400))
+    
+    matching_bike = Bike.query.get(bike_id)
+
+    if matching_bike is None:
+        response_str = f"Bike with id `{bike_id}` was not found in the database."
+        abort(make_response(jsonify({"message":response_str}), 400))
+    
+    return matching_bike
 
 @bike_bp.route("", methods=["POST"])
 def add_bike():
@@ -36,31 +51,48 @@ def get_all_bikes():
         response.append(bike_dict)
     return jsonify(response), 200
 
-# @bike_bp.route("/<bike_id>", methods=["GET"])
-# def get_one_bike(bike_id):
-#     #see if bike_id can be converted to an integer
-#     #try-except: try to convert to an int, if error occurs, catch it and raise 400 error with message
-#     try:
-#         bike_id = int(bike_id)
-#     except ValueError:
-#         response_str = f"Invalid bike_id: `{bike_id}`. ID must be an integer"
-#         return jsonify({"message": response_str}), 400
-#     #after the try-except: bike_id will be a valid int
+#October 31
+#Add DELETE, PUT, and GET(one bike)
 
-#     #looping through data to find a bike with matching bike_id
-#     #if found: return that bikes data with 200 response code
-#     for bike in bikes:
-#         if bike.id == bike_id:
-#             bike_dict = {
-#                 "id": bike.id,
-#                 "name": bike.name,
-#                 "price": bike.price,
-#                 "size": bike.size,
-#                 "type": bike.type
-#             }
-#             #return in the if block
-#             return jsonify(bike_dict), 200
+@bike_bp.route("/<bike_id>", methods=["GET"])
+def get_one_bike(bike_id):
+    chosen_bike = get_one_bike_or_abort(bike_id)
+    bike_dict = {
+        "id": chosen_bike.id,
+        "name": chosen_bike.name,
+        "price": chosen_bike.price,
+        "size": chosen_bike.size,
+        "type": chosen_bike.type
+    }
 
-#     #after the loop: the bike with matching bike_id was not found, we will raise a 404 error with message
-#     response_message = f"bike_id: {bike_id} not found."
-#     return jsonify({"message": response_message}), 404
+    return jsonify(bike_dict), 200
+
+@bike_bp.route("/<bike_id>", methods=["PUT"])
+def update_bike_with_new_vals(bike_id):
+    chosen_bike = get_one_bike_or_abort(bike_id)
+
+    request_body = request.get_json()
+
+    if "name" not in request_body or \
+        "size" not in request_body or \
+        "price" not in request_body or \
+        "type" not in request_body:
+            return jsonify({"message": "Request must include name, size, price, and type"}), 400
+    
+    chosen_bike.name = request_body["name"]
+    chosen_bike.size = request_body["size"]
+    chosen_bike.price = request_body["price"]
+    chosen_bike.type = request_body["type"]
+
+    db.session.commit()
+
+    return jsonify({"message": f"Successfully replaced bike with id `{bike_id}`"}), 200
+
+@bike_bp.route("/<bike_id>", methods=["DELETE"])
+def delete_one_bike(bike_id):
+    chosen_bike = get_one_bike_or_abort(bike_id)
+    db.session.delete(chosen_bike)
+
+    db.session.commit()
+
+    return jsonify({"message": f"Successfully deleted bike with id `{bike_id}`"}), 200
