301bbf360b2a35a9cb88ecb446b587c2038af497
Samantha Hunt <ariaandsam@gmail.com>
2022-11-07 10:54:06 -0800

    flask

--- app/models/bike.py
+++ app/models/bike.py
@@ -6,3 +6,23 @@ class Bike(db.Model):
     price = db.Column(db.Integer)
     size = db.Column(db.Integer)
     type = db.Column(db.String)
+
+    def to_dict(self):
+        bike_dict = {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "size": self.size,
+            "type": self.type
+        }
+        return bike_dict
+
+    @classmethod
+    def from_dict(cls, data_dict):
+        if "name" in data_dict and "price" in data_dict and "size" in data_dict and "type" in data_dict:
+            new_obj=cls(name=data_dict["name"],
+            price=data_dict["price"],
+            size=data_dict["size"],
+            type=data_dict["type"])
+
+            return new_obj
--- app/routes/bike.py
+++ app/routes/bike.py
@@ -1,35 +1,16 @@
 from flask import Blueprint, jsonify, request, abort, make_response
 from app import db
 from app.models.bike import Bike
-
+from .routes_helper import get_one_obj_or_abort
 
 bike_bp = Blueprint("bike_bp", __name__, url_prefix="/bike")
 
-def get_one_bike_or_abort(bike_id):
-    try:
-        bike_id = int(bike_id)
-    except ValueError:
-        response_str = f"Invalid bike_id: `{bike_id}`. ID must be an integer."
-        abort(make_response(jsonify({"message":response_str}), 400))
-    
-    matching_bike = Bike.query.get(bike_id)
-
-    if matching_bike is None:
-        response_str = f"Bike with id `{bike_id}` was not found in the database."
-        abort(make_response(jsonify({"message":response_str}), 404))
-    
-    return matching_bike
 
 @bike_bp.route("", methods=["POST"])
 def add_bike():
     request_body = request.get_json()
 
-    new_bike = Bike(
-        name=request_body["name"],
-        price=request_body["price"],
-        size=request_body["size"],
-        type=request_body["type"]
-    )
+    new_bike = Bike.from_dict(request_body)
 
     db.session.add(new_bike)
     db.session.commit()
@@ -45,16 +26,8 @@ def get_all_bikes():
     else:
         bikes = Bike.query.filter_by(name=name_param)
 
-    response = []
-    for bike in bikes:
-        bike_dict = {
-            "id": bike.id,
-            "name": bike.name,
-            "price": bike.price,
-            "size": bike.size,
-            "type": bike.type
-        }
-        response.append(bike_dict)
+    
+    response = [bike.to_dict() for bike in bikes]
     return jsonify(response), 200
 
 #October 31
@@ -62,20 +35,15 @@ def get_all_bikes():
 
 @bike_bp.route("/<bike_id>", methods=["GET"])
 def get_one_bike(bike_id):
-    chosen_bike = get_one_bike_or_abort(bike_id)
-    bike_dict = {
-        "id": chosen_bike.id,
-        "name": chosen_bike.name,
-        "price": chosen_bike.price,
-        "size": chosen_bike.size,
-        "type": chosen_bike.type
-    }
+    chosen_bike = get_one_obj_or_abort(Bike, bike_id)
+
+    bike_dict = chosen_bike.to_dict()
 
     return jsonify(bike_dict), 200
 
 @bike_bp.route("/<bike_id>", methods=["PUT"])
 def update_bike_with_new_vals(bike_id):
-    chosen_bike = get_one_bike_or_abort(bike_id)
+    chosen_bike = get_one_obj_or_abort(Bike, bike_id)
 
     request_body = request.get_json()
 
@@ -96,7 +64,7 @@ def update_bike_with_new_vals(bike_id):
 
 @bike_bp.route("/<bike_id>", methods=["DELETE"])
 def delete_one_bike(bike_id):
-    chosen_bike = get_one_bike_or_abort(bike_id)
+    chosen_bike = get_one_obj_or_abort(Bike, bike_id)
     db.session.delete(chosen_bike)
 
     db.session.commit()
